ec20682f522021d85d445a3fd5d5d9aa93f5581c
yashchandak101 <chandak.yash101@gmail.com>
2025-10-04 07:52:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], // add your cloud name here
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "crappylife-frontend.vercel.app/", // Replace with your Render backend domain
+        pathname: "/media/**",
+      },
+    ],
   },
 };
 
