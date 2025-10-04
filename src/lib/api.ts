c7f98a5cdabf08111008039ae156b1139c952f03
yashchandak101 <chandak.yash101@gmail.com>
2025-10-04 18:06:34 +0530

    Delete src directory

--- src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/api.ts
-export async function apiFetch(url: string, options?: RequestInit) {
-  const res = await fetch(url, options)
-  if (!res.ok) {
-    throw new Error(`API request failed: ${res.status}`)
-  }
-  return res.json()
-}
-
