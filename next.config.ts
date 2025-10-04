1bd44a1e3aaefd41edad4ccc1b9a757014709e2a
yashchandak101 <chandak.yash101@gmail.com>
2025-10-04 07:32:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/media/**",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/media/**",
-      },
-    ],
+    domains: ['res.cloudinary.com'], // add your cloud name here
   },
 };
 
